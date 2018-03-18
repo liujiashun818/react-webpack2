b809e3e00e639190e538a3f55f2bd54a2285d39b
liujiashun818 <1696054400@qq.com>
2018-03-18 22:06:40 +0800

    img

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /*eslint-disable*/
 import React, { Component } from 'react';
 import './app.less';
-
+import a from './images/no.png'
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -13,11 +13,15 @@ export default class App extends Component {
   add () {
     this.setState({ count: this.state.count + 1 });
   };
+
   render() {
     return (
       <div>
       <h1>{this.state.count}</h1>
-      <h1 className='what'>111111坎坎坷坷扩扩</h1>
+      <div className='what '>
+        <img src={a} alt="wo" />
+      </div>
+      <div className='what what2 '>111111坎坎坷坷扩扩</div>
     <button onClick={() => this.add()}>增加1</button>
     </div>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,9 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-var autoprefixer = require('autoprefixer');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const website = { publicPath:'http://localhost:8080/' };
 
 module.exports = {
   entry: [
@@ -14,7 +16,8 @@ module.exports = {
   ],
     output: {
     path: path.resolve(__dirname, './dist'), // 输出的路径
-    filename: '[name]_[hash:8].js'  // 打包后文件
+    filename: '[name]_[hash:8].js',  // 打包后文件
+    publicPath: website.publicPath
   },
   module: {
     rules: [
@@ -24,12 +27,23 @@ module.exports = {
         exclude: /node_modules/
       },
       {
+        test:/\.(png|jpg|gif|jpeg)$/,
+        use:[{
+          loader:'url-loader',
+          options:{
+            limit:500000,
+            outputPath:'images/'
+          }
+          }]
+      },
+      {
         enforce: "pre",
         test: /\.(js|jsx)$/,
         loader: 'eslint-loader',
         exclude: /node_modules/
       },{
         test: /\.css$/,
+        exclude: /^node_modules$/,
         use: [{
           loader: 'style-loader'
         }, {
@@ -38,24 +52,36 @@ module.exports = {
       },
       {
         test: /\.less$/,
+        exclude: /^node_modules$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: [
-            'css-loader',
-            'postcss-loader',
-            'less-loader'
-          ]
+          use: [{
+            loader: 'css-loader'
+          },
+            {
+            loader: 'postcss-loader'
+          },
+            {
+            loader: 'less-loader'
+          }],
         })
       },
     ]
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new ExtractTextPlugin("styles.css"),
+    new ExtractTextPlugin("styles_[hash:8].css"),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, './src/index.template.html'),
       inject: true
-    })
-  ],
-  // postcss: [autoprefixer({ browsers: ['last 2 versions']})]
+    }),
+    new CleanWebpackPlugin(
+      ['dist/main_*.js','dist/styles_*css','index.html'],　 //匹配删除的文件
+      {
+        root: __dirname,       　　　　　　　　　　//根目录
+        verbose:  true,        　　　　　　　　　　//开启在控制台输出信息
+        dry:      false        　　　　　　　　　　//启用删除文件
+      }
+    )
+  ]
 }
